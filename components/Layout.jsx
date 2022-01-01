82b393b7d05f7cdcbe82d4e3a00edb02eb4052b0
PhyoWaiNyunt <50352633+pwn-69@users.noreply.github.com>
2022-01-01 15:55:35 +0630

    finished project

---
+++ components/Layout.jsx
@@ -0,0 +1,22 @@
+// navbar content footbar
+import React from "react";
+import Head from "next/head";
+import { Box } from "@chakra-ui/react";
+
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => (
+  <>
+    <Head>
+      <title>Blue Sky Realty</title>
+    </Head>
+    <Box maxWidth="1280px" m="auto">
+      <header><Navbar/></header>
+      <main>{children}</main>
+      <footer><Footer /></footer>
+    </Box>
+  </>
+);
+
+export default Layout;
